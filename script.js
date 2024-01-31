19df2f587838030f3ce5c4125a20bc3c572d9158
collins Erim <collinkesh061@gmail.com>
2024-01-31 01:44:54 +0100

    great commit

---
+++ script.js
@@ -0,0 +1,74 @@
+
+
+function showButton() {
+    const textToShow = document.querySelector('#textToShow');
+    const showButtonImg = document.querySelector('#showButtonImg');
+    const hideButtonImg = document.querySelector('#hideButtonImg');
+    textToShow.style.display = 'flex';
+    showButtonImg.style.display = 'none';
+    hideButtonImg.style.display = 'block';
+}
+
+function closeButton() {
+    const textToShow = document.querySelector('#textToShow');
+    const showButtonImg = document.querySelector('#showButtonImg');
+    const hideButtonImg = document.querySelector('#hideButtonImg');
+    textToShow.style.display = 'none';
+    showButtonImg.style.display = 'block';
+    hideButtonImg.style.display = 'none';
+};
+
+function showButtonTwo() {
+    const textToShowTwo = document.querySelector('#textToShowTwo');
+    const showButtonImgTwo = document.querySelector('#showButtonImgTwo');
+    const hideButtonImgTwo = document.querySelector('#hideButtonImgTwo');
+    textToShowTwo.style.display = 'flex';
+    showButtonImgTwo.style.display = 'none';
+    hideButtonImgTwo.style.display = 'block';
+}
+
+function closeButtonTwo() {
+    const textToShowTwo = document.querySelector('#textToShowTwo');
+    const showButtonImgTwo = document.querySelector('#showButtonImgTwo');
+    const hideButtonImgTwo = document.querySelector('#hideButtonImgTwo');
+    textToShowTwo.style.display = 'none';
+    showButtonImgTwo.style.display = 'block';
+    hideButtonImgTwo.style.display = 'none';
+};
+
+function showButtonThree() {
+    const textToShowThree = document.querySelector('#textToShowThree');
+    const showButtonImgThree = document.querySelector('#showButtonImgThree');
+    const hideButtonImgThree = document.querySelector('#hideButtonImgThree');
+    textToShowThree.style.display = 'flex';
+    showButtonImgThree.style.display = 'none';
+    hideButtonImgThree.style.display = 'block';
+}
+
+function closeButtonThree() {
+    const textToShowThree = document.querySelector('#textToShowThree');
+    const showButtonImgThree = document.querySelector('#showButtonImgThree');
+    const hideButtonImgThree = document.querySelector('#hideButtonImgThree');
+    textToShowThree.style.display = 'none';
+    showButtonImgThree.style.display = 'block';
+    hideButtonImgThree.style.display = 'none';
+}
+
+
+function showButtonFour() {
+   const paragraphFour = document.querySelector('#paragraphFour');
+   const showButtonFour = document.querySelector('#showButtonFour');
+   const hideButtonFour = document.querySelector('#hideButtonFour');
+   paragraphFour.style.display = 'block';
+   showButtonFour.style.display = 'none';
+   hideButtonFour.style.display = 'block';
+}
+
+function hideButtonFour() {
+    const paragraphFour = document.querySelector('#paragraphFour');
+    const showButtonFour = document.querySelector('#showButtonFour');
+    const hideButtonFour = document.querySelector('#hideButtonFour');
+    paragraphFour.style.display = 'none';
+    showButtonFour.style.display = 'block';
+    hideButtonFour.style.display = 'none';
+}
